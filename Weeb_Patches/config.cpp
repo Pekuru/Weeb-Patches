c166d7f66852f9625b8d3be6944ad8592648b09c
Pekuru <9923839+Pekuru@users.noreply.github.com>
2022-07-29 16:37:23 +0200

    dump

---
+++ Weeb_Patches/config.cpp
@@ -0,0 +1,88 @@
+class defaultUserActions;
+class DefaultEventhandlers;
+class CfgPatches
+{
+	class Weeb_patches
+	{
+		units[]={};
+		weapons[]={};
+		requiredVersion=0.1;
+		requiredAddons[]={};
+	};
+};
+
+class CfgUnitInsignia
+{
+    class p_weeb_1
+    {
+        displayName = "Patch #1";
+        author = "Pek";
+        texture = "\Weeb_Patches\data\patches\weeb1.paa";
+    };
+    class p_weeb_2
+    {
+        displayName = "Patch #2";
+        author = "Pek";
+        texture = "\Weeb_Patches\data\patches\weeb2.paa";
+    };
+    class p_weeb_3
+    {
+        displayName = "Patch #3";
+        author = "Pek";
+        texture = "\Weeb_Patches\data\patches\weeb3.paa";
+    };
+    class p_weeb_4
+    {
+        displayName = "Patch #4";
+        author = "Pek";
+        texture = "\Weeb_Patches\data\patches\weeb4.paa";
+    };
+    class p_weeb_5
+    {
+        displayName = "Patch #5";
+        author = "Pek";
+        texture = "\Weeb_Patches\data\patches\weeb5.paa";
+    };
+    class p_weeb_6
+    {
+        displayName = "Patch #6";
+        author = "Pek";
+        texture = "\Weeb_Patches\data\patches\weeb6.paa";
+    };
+    class p_weeb_7
+    {
+        displayName = "Patch #7";
+        author = "Pek";
+        texture = "\Weeb_Patches\data\patches\weeb7.paa";
+    };
+    class p_weeb_8
+    {
+        displayName = "Patch #8";
+        author = "Pek";
+        texture = "\Weeb_Patches\data\patches\weeb8.paa";
+    };
+    class p_weeb_9
+    {
+        displayName = "Patch #9";
+        author = "Pek";
+        texture = "\Weeb_Patches\data\patches\weeb9.paa";
+    };
+    class p_weeb_10
+    {
+        displayName = "Patch #10";
+        author = "Pek";
+        texture = "\Weeb_Patches\data\patches\weeb10.paa";
+    };
+    class p_weeb_11
+    {
+        displayName = "Patch #11";
+        author = "Pek";
+        texture = "\Weeb_Patches\data\patches\weeb11.paa";
+    };
+    class p_weeb_12
+    {
+        displayName = "Patch #12";
+        author = "Pek";
+        texture = "\Weeb_Patches\data\patches\weeb12.paa";
+    };
+};
